45004b1e0c215713078a86d14fb7710ba011d6b8
unknown <brahim.ezzali@hotmail.com>
2022-02-14 13:21:51 +0100

    Create Auth Gard to controlle Access

---
+++ src/app/services/auth/auth.gard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGard implements CanActivate , CanLoad{
+    
+
+    constructor(private authService : AuthService , private router : Router){}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        if(this.authService.isAuth()){
+            return true;
+        }else{
+            return this.router.navigate(['/login']);
+        } 
+    }
+
+    canLoad(route: Route): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        if(this.authService.isAuth()){
+            return true;
+        }else{
+            return this.router.navigate(['/login']);
+        }
+    }
+}
