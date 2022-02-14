3241949249b94d13404ab12c940664ff062ccb58
unknown <brahim.ezzali@hotmail.com>
2022-02-14 13:39:24 +0100

    Create App Module that contains all Modules used in the App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
-
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AuthService } from './services/auth/auth.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+import { UIService } from './seared/ui.service';
+import { AuthModule } from './modules/auth.module';
+import { TrainingService } from './services/training/training.service';
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
+import { AuthRoutingModule } from './routes/auth-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/app.reducer';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    MaterialModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    MaterialModule,
+    AuthModule,
+    AuthRoutingModule,
+    StoreModule.forRoot(reducers)
   ],
-  providers: [],
+  providers: [AuthService , UIService , TrainingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
