f3d85fc806e2b5040bfc281ace3e79f8405fdfa3
unknown <brahim.ezzali@hotmail.com>
2022-03-09 12:48:23 +0100

    clean the code & use getIsLoading and getAvailabelExercises selectors

--- src/app/components/training/new-training/new-training.component.ts
+++ src/app/components/training/new-training/new-training.component.ts
@@ -1,52 +1,34 @@
-import { Component, OnDestroy, OnInit} from '@angular/core';
-import {  NgForm  } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
-import { UIService } from 'src/app/seared/ui.service';
+import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Exercise } from 'src/app/services/training/exercise.model';
 import { TrainingService } from 'src/app/services/training/training.service';
+import * as TrainingReducer from '../../../store/training/training.reducer';
+import * as RootReducer from '../../../store/app.reducer'
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
-export class NewTrainingComponent implements OnInit , OnDestroy{
-  exercises : Exercise[] ;
-  subscription : Subscription;
-  selectedExercise : Exercise;
-  isLoading : boolean = false;
-  isLoadingSubscription : Subscription;
+export class NewTrainingComponent implements OnInit{
+  exercises$ : Observable<Exercise[]> ;
+  isLoading$ : Observable<boolean> ;
 
   constructor(private trainingService : TrainingService , 
-              private uiService : UIService) 
-    { 
-
-    }
+              private store : Store<TrainingReducer.State> ) 
+    { }
 
   ngOnInit(): void {
-    this.isLoadingSubscription = this.uiService.isLoading.subscribe(isLoad => this.isLoading = isLoad);
-    console.log('this.isLoading : ' , this.isLoading);
-    this.subscription  = this.trainingService.exercisesChanged.subscribe((exercises)=>{
-      this.exercises = exercises;
-    });
+    this.isLoading$ = this.store.select(RootReducer.getIsLoading);
+    this.exercises$ = this.store.select(TrainingReducer.getAvailabelExercises)
     this.fetchExercises();
   }
   
   fetchExercises(){
     this.trainingService.fetchAvailabelExercises();
   }
-
-  ngOnDestroy(): void {
-    if(this.subscription){
-      this.subscription.unsubscribe();
-    }
-    
-    if(this.isLoadingSubscription){
-      this.isLoadingSubscription.unsubscribe();
-    }
-  }
-
-
 
   StartTrainig(form : NgForm){
     this.trainingService.startExercise(form.value.selectedExercise);
