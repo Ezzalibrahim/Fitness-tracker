38ae492781206bbec8ce512081e74f3b9c930c08
unknown <brahim.ezzali@hotmail.com>
2022-03-09 13:01:54 +0100

    add StoreModule and Training Reducer

--- src/app/modules/training.module.ts
+++ src/app/modules/training.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 
 import { ReactiveFormsModule} from "@angular/forms";
-
+import { StoreModule } from "@ngrx/store";
 import { TrainingComponent } from '../components/training/training.component';
 import { CurrentTrainingComponent } from '../components/training/current-training/current-training.component';
 import { NewTrainingComponent } from '../components/training/new-training/new-training.component';
 import { PastTrainingComponent } from '../components/training/past-training/past-training.component';
 import { StopTrainingComponent } from "../components/training/current-training/stop-training.component";
-import { SharedModule } from "./seared.module";
+import { SharedModule } from "./shared.module";
 import { TrainingRoutingModule } from "../routes/training-routing.module";
+import { trainingReducer } from "../store/training/training.reducer";
 
 @NgModule({
     declarations : [
@@ -21,7 +22,8 @@ import { TrainingRoutingModule } from "../routes/training-routing.module";
     imports : [
         SharedModule,
         ReactiveFormsModule,
-        TrainingRoutingModule
+        TrainingRoutingModule,
+        StoreModule.forFeature('training' , trainingReducer)
     ],
     entryComponents : [StopTrainingComponent],
 
