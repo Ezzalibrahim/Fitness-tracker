cbcd4e647d930a654afada755efaf5b137167ea7
unknown <brahim.ezzali@hotmail.com>
2022-03-09 13:15:31 +0100

    clean the code & use Auth dispatchers Store to register & login & initAuth

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,39 +1,36 @@
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { User } from './user.model';
 import { AuthData } from './authData.model'
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { TrainingService } from '../training/training.service';
-import { UIService } from 'src/app/seared/ui.service';
+import { UIService } from 'src/app/shared/ui.service';
 import { Store } from '@ngrx/store';
-import * as StoreApp from '../../store/app.reducer';
-import { StartLoading, StopLoading } from 'src/app/seared/ui.action';
+import * as RootReducer from '../../store/app.reducer';
+import * as AuthActions from '../../store/auth/auth.action';
+import { StartLoading, StopLoading } from 'src/app/shared/ui.action';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  authChange : Subject<boolean> = new Subject();
-  private user : User ;
 
   constructor(
     private  router : Router, 
     private fireAuth: AngularFireAuth , 
     private trainingService : TrainingService,
     private uiService : UIService,
-    private store : Store<StoreApp.State>)
+    private store : Store<RootReducer.State>)
     { }
   
   initAuthListener(){
     this.fireAuth.authState.subscribe(user =>{
       if (user) {
-        this.authChange.next(true);
+        this.store.dispatch(new AuthActions.UserAuthenticated())
         this.router.navigate(['/training']);
       } else {
         this.trainingService.cancelSubscription();
-        this.user = null;
-        this.authChange.next(false);
+        this.store.dispatch(new AuthActions.UserUnAuthenticated());
         this.router.navigate(['/login']);
       }
     })
@@ -62,11 +59,6 @@ export class AuthService {
     this.fireAuth
       .signInWithEmailAndPassword( authData.email , authData.password)
       .then(result =>{
-        this.user = {
-          email : result.user.email,
-          userId : result.user.uid
-        }
-        console.log(result);
         this.store.dispatch(new StopLoading());
       })
       .catch(err => 
@@ -77,16 +69,12 @@ export class AuthService {
       );
   }
 
-  getUser() {
-    return {...this.user};
-  }
 
   logOutUser(){
     this.fireAuth.signOut();
+    this.store.dispatch(new AuthActions.UserUnAuthenticated());
   }
 
-  isAuth(){
-    return this.user != null;
-  }
+
 
 }
