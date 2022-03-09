ce49b85e9cc7840085e193e28293ff1682cec27f
unknown <brahim.ezzali@hotmail.com>
2022-03-09 12:35:19 +0100

    clean the code & use getIsAuth selector

--- src/app/components/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/components/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,28 +1,23 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, EventEmitter , OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import  * as RootStore from '../../../store/app.reducer';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit , OnDestroy{
+export class SidenavListComponent implements OnInit{
   @Output() onToggle = new EventEmitter();
-  subscription : Subscription = new Subscription();
-  isAuth : boolean = false;
+  isAuth$ : Observable<boolean> ;
 
-  constructor(private authService : AuthService) { }
+  constructor(private authService : AuthService , 
+              private store : Store < RootStore.State >) { }
 
   ngOnInit(): void {
-    this.subscription = this.authService.authChange.subscribe((authStatus)=>{
-      this.isAuth = authStatus;
-    })
-  }
-  ngOnDestroy(): void {
-    if(this.subscription){
-      this.subscription.unsubscribe();
-    }
+    this.isAuth$ = this.store.select(RootStore.getIsAuth);
   }
 
   onToggleSidenav(){
