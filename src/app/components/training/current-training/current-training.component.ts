bda9c4c643b71799fe55dfe26b82d56d676f5725
unknown <brahim.ezzali@hotmail.com>
2022-03-09 12:44:24 +0100

    clean the code & use getRuniningExercise selector & use some operators is rxjs

--- src/app/components/training/current-training/current-training.component.ts
+++ src/app/components/training/current-training/current-training.component.ts
@@ -3,6 +3,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { Exercise } from 'src/app/services/training/exercise.model';
 import { TrainingService } from 'src/app/services/training/training.service';
 import { StopTrainingComponent } from './stop-training.component';
+import * as TrainingState from '../../../store/training/training.reducer';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-current-training',
@@ -10,25 +13,35 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  selectedExercise : Exercise ;
+  selectedExercise : Exercise;
   progress : number = 0;
   timer ;
-  constructor(private dialog : MatDialog , private trainingService : TrainingService) { }
+  constructor(private dialog : MatDialog ,
+              private trainingService : TrainingService ,
+              private store : Store<TrainingState.State>) { }
 
   ngOnInit(): void {
-    this.selectedExercise = this.trainingService.getRunningExercise();
-    this.stopOrResumeTimerProgress();
+    this.startOrResumeTimerProgress();
   }
 
-  stopOrResumeTimerProgress(){
-    this.timer = setInterval(()=>{
-      this.progress += 1;
-      if (this.progress >= 100) {
-        this.trainingService.completeExercise();
-        clearInterval(this.timer);
-      }
-    },this.selectedExercise.duration * 3)
-  }
+  startOrResumeTimerProgress(){
+    this.store
+      .select(TrainingState.getRuningExercise)
+      .pipe(take(1))
+      .subscribe(selectedExercise  => {
+        this.selectedExercise = selectedExercise;
+        const step = (selectedExercise.duration / 100) * 1000;
+
+        console.log("Duration : ", selectedExercise.duration);
+        this.timer = setInterval(()=>{
+          this.progress += 1;
+          if (this.progress >= 100) {
+            this.trainingService.completeExercise();
+            clearInterval(this.timer);
+          }
+        }, 100);
+    });
+  }  
 
   onStop(){
     clearInterval(this.timer);
@@ -42,7 +55,7 @@ export class CurrentTrainingComponent implements OnInit {
       if (result && this.progress < 100) {
         this.trainingService.stopExercise(this.progress);
       } else {
-        this.stopOrResumeTimerProgress();
+        this.startOrResumeTimerProgress();
       }
     });
 
